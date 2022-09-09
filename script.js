c67542d13c063c1fd2212aba270aee247538a110
Victoria El Bedewi <victoriaelbedewi@laptop-gvk5i8oi.home>
2022-09-10 01:32:34 +0300

    add event text functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var day = today.format("[Today is] dddd");
 var  time = today.format("hh:mm a");
 $("#currentDay").text(`${day} the time is: ${time}`);
 var timeSlots = [$("#nine"),$("#ten"),$("#eleven"),$("#twelve"),$("#one"),$("#two"),$("#three"),$("#four"),$("#five")];
+// gets and displays day and time on screen
 function getTime(){
      today = moment();
      day = today.format("[Today is] dddd");
@@ -10,11 +11,12 @@ function getTime(){
     $("#currentDay").text(`${day} the time is: ${time}`);
     // console.log(today.format("hh a"));
 }
-
+// change time every minute
 setInterval(getTime,60000)
 
 var currentHour;
 var eventHour;
+// color codes the planner (red for past, gray for present, green for future)
 function changeColor(){
     currentHour = parseInt(today.format("HH"));
     for (let index = 0; index < timeSlots.length; index++) {
@@ -36,4 +38,26 @@ function changeColor(){
 }
 changeColor();
 setInterval(changeColor,60000);
+
+// adding an event
+var inputText; 
+function addEvent(event){
+    var clicked = event.target;
+    
+            if (clicked.getAttribute("class") === "textHere"){
+               inputText = document.createElement("textarea");
+               inputText.style.width="800px";
+                clicked.appendChild(inputText);
+                
+            }
+}
+// saving to local storage
+function saveEvent(event){
+    if(event.target.getAttribute("class") === "btn"){
+        console.log("clicked save");
+   var rowID = event.target.parent("tr").getAttribute("id");
+   console.log(rowID);
+}}
+document.addEventListener("click",addEvent);
+document.addEventListener("click",saveEvent);
 
